69e0f1995c516aa43a4446a36eb0fb5617a6a09a
Yuta Saotome <42575295+utiuti12345@users.noreply.github.com>
2021-03-31 23:34:46 +0900

    Domainモデルを作成

---
+++ src/domain/wish.ts
@@ -0,0 +1,36 @@
+import {v4 as generatedUuid} from "react-native-uuid";
+import {assertIsDefined} from "../lib/assert";
+
+export interface Model {
+    readonly id:string;
+    readonly title: string;
+    readonly imageUrl?:string;
+    readonly detail?: string;
+    readonly price?: number;
+    readonly createdAt:string;
+    readonly updateAt:string;
+}
+
+export interface Values {
+    readonly title:string;
+    readonly imageUrl?:string;
+    readonly detail?: string;
+    readonly price?: number;
+}
+
+export function factory(wish:Values):Model {
+    assertIsDefined(wish.title);
+
+    const now = new Date().toISOString();
+
+    return{
+        id:generatedUuid(),
+        title:wish.title,
+        imageUrl:wish.imageUrl,
+        detail:wish.detail,
+        price:wish.price,
+        createdAt:now,
+        updateAt:now,
+    }
+}
+
