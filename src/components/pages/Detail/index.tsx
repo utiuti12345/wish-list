37f7e4a699cc4b6dcef6b028256fdec65121687e
Yuta Saotome <42575295+utiuti12345@users.noreply.github.com>
2021-04-09 18:18:26 +0900

    Detailに入力フォームを追加

--- src/components/pages/Detail/index.tsx
+++ src/components/pages/Detail/index.tsx
@@ -1,6 +1,18 @@
-import React from "react";
-import {SafeAreaView, Text} from "react-native";
+import React, {useCallback, useState} from "react";
+import {Image, SafeAreaView, StyleSheet, Text, TextInput} from "react-native";
 import {RouteProp, useRoute} from "@react-navigation/native";
+
+const styles = StyleSheet.create({
+    image:{
+        width:300,
+        height:300,
+        borderRadius:10,
+        borderWidth:2,
+        borderColor:'#d35647',
+        resizeMode:'contain',
+        margin:8
+    }
+});
 
 interface Params{
     id:string;
@@ -10,11 +22,20 @@ interface Params{
 
 export default function Detail() {
     const {params} = useRoute<RouteProp<Record<string, Params>, string>>();
-    const {title} = params;
+    const {id,title,imageUrl} = params;
+
+    const [value,setValue] = useState(title);
+    const onChange = useCallback((newValue) => {
+        setValue(newValue);
+    },[setValue]);
 
     return (
         <SafeAreaView>
-            <Text>{title}</Text>
+            <TextInput value={value} onChangeText={onChange}></TextInput>
+            <Image source={{ uri: imageUrl }}
+                   key={id}
+                   style={styles.image}
+            />
         </SafeAreaView>
     )
 }
