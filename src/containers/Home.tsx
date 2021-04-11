38ffde672e0c71e424547a78fc4c0ae9360a9300
Yuta Saotome <42575295+utiuti12345@users.noreply.github.com>
2021-04-11 22:11:40 +0900

    containerのコメントアウトを解除

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,12 +1,12 @@
-// import React from 'react';
-// import {useDispatch, useSelector} from 'react-redux';
-// import * as Wish from '../modules/wish';
-//
-// import {Home} from '../components/pages';
-// import {getWishList} from "../selectors/wish";
-//
-// export default function ConnectedHome(){
-//     const wishList = useSelector(getWishList);
-//
-//     return <Home wishList={wishList}/>
-// }
+import React from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import * as Wish from '../modules/wish';
+
+import {Home} from '../components/pages';
+import {getWishList} from "../selectors/wish";
+
+export default function ConnectedHome(){
+    const wishList = useSelector(getWishList);
+
+    return <Home wishList={wishList}/>
+}
