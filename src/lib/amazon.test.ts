6231b4d3f80a41912b011633e2b274b23355bf0d
Yuta Saotome <42575295+utiuti12345@users.noreply.github.com>
2021-04-15 23:54:15 +0900

    amazonのURLから画像を取得する処理を修正

--- src/lib/amazon.test.ts
+++ src/lib/amazon.test.ts
@@ -1,10 +1,30 @@
 import * as amazon from "./amazon";
 
 describe('Amazon',() =>{
-   describe("Amazon Image Url Get",() =>{
-       it('Amazon Image Url Get', () => {
+   describe("Amazonの画像URLを取得する",() =>{
+       it('/dp/******のパターン', () => {
            const imageUrl = amazon.fetchImageUrl("https://www.amazon.co.jp/dp/B01BM6FQQS/ref=cm_sw_r_cp_api_i_7NXSD1FGD62V9GRRSHFQ");
-           expect(imageUrl).toBe("https://www.amazon.co.jp/dp/B01BM6FQQS/ref=cm_sw_r_cp_api_i_7NXSD1FGD62V9GRRSHFQ");
+           expect(imageUrl).toBe("https://images-na.ssl-images-amazon.com/images/P/B01BM6FQQS.09.MZZZZZZZ");
+       });
+
+       it('/dp/******で複雑なURLパターン', () => {
+           const imageUrl = amazon.fetchImageUrl("https://www.amazon.co.jp/SwitchBot-%E3%82%B9%E3%82%A4%E3%83%83%E3%83%81%E3%83%9C%E3%83%83%E3%83%88-%E3%82%B9%E3%83%9E%E3%83%BC%E3%83%88%E3%83%9B%E3%83%BC%E3%83%A0-%E5%AD%A6%E7%BF%92%E3%83%AA%E3%83%A2%E3%82%B3%E3%83%B3-Alexa/dp/B07TTH5TMW/ref=lp_6257144051_1_1?s=specialty-aps");
+           expect(imageUrl).toBe("https://images-na.ssl-images-amazon.com/images/P/B07TTH5TMW.09.MZZZZZZZ");
+       });
+
+       it('/exec/obidos/ASIN/******のパターン', () => {
+           const imageUrl = amazon.fetchImageUrl("https://www.amazon.co.jp/exec/obidos/ASIN/4163741100/mersys-22/ref=nosim");
+           expect(imageUrl).toBe("https://images-na.ssl-images-amazon.com/images/P/4163741100.09.MZZZZZZZ");
+       });
+
+       it('/gp/product/******のパターン', () => {
+           const imageUrl = amazon.fetchImageUrl("https://www.amazon.co.jp/gp/product/B004W6ISOI");
+           expect(imageUrl).toBe("https://images-na.ssl-images-amazon.com/images/P/B004W6ISOI.09.MZZZZZZZ");
+       });
+
+       it('/o/ASIN/******のパターン', () => {
+           const imageUrl = amazon.fetchImageUrl("https://www.amazon.co.jp/o/ASIN/B004W6ISOI");
+           expect(imageUrl).toBe("https://images-na.ssl-images-amazon.com/images/P/B004W6ISOI.09.MZZZZZZZ");
        });
    });
 });
--- src/lib/amazon.ts
+++ src/lib/amazon.ts
@@ -1,10 +1,15 @@
-const regexp = /[^0-9A-Z]([0-9A-Z]{10})([^0-9A-Z]|$)/;
+const regexp = /\/(dp|gp\/product|ASIN|exec\/obidos\/ASIN|o\/ASIN)\/[\d\w]+/g;
+const replace = /\/(dp|gp\/product|ASIN|exec\/obidos\/ASIN|o\/ASIN)\//;
 
 export function fetchImageUrl(url:string){
     if(!url){
         return "";
     }
 
-    const asinCode = url.match(regexp);
-    return `https://images-na.ssl-images-amazon.com/images/P/${asinCode}.09.MZZZZZZZ`
+    const patternMatch = url.match(regexp);
+    if(patternMatch !== null){
+        const asin = patternMatch[0].replace(replace,'');
+        return `https://images-na.ssl-images-amazon.com/images/P/${asin}.09.MZZZZZZZ`
+    }
+    return "";
 }
