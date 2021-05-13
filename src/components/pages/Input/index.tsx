79a690244270c6a9e7b4a062d3dfa7c340be4ed3
Yuta Saotome <42575295+utiuti12345@users.noreply.github.com>
2021-05-13 23:20:54 +0900

    Inputのデザイン修正

--- src/components/pages/Input/index.tsx
+++ src/components/pages/Input/index.tsx
@@ -1,5 +1,5 @@
 import React, {useCallback, useContext, useState} from "react";
-import {Image, SafeAreaView, StyleSheet, Text, View} from "react-native";
+import {Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import {Avatar, Button, dismiss, TextField} from "../../atoms";
 import {fetchImageUrl} from "../../../lib/amazon";
 import {useNavigation} from "@react-navigation/native";
@@ -9,6 +9,7 @@ import {TouchableWithoutFeedback} from "@ui-kitten/components/devsupport";
 import {COLOR} from "../../../constants/theme";
 import {IconButton} from "../../molecules";
 import {UserContext} from "../../../contexts";
+import * as ImagePicker from "expo-image-picker";
 
 const styles = StyleSheet.create({
     container: {
@@ -27,6 +28,10 @@ const styles = StyleSheet.create({
     inputContainer:{
 
     },
+    imageContainer:{
+        alignSelf: "center",
+        flexDirection:"row"
+    },
     text:{
         borderRadius: 25,
     },
@@ -35,14 +40,14 @@ const styles = StyleSheet.create({
         height: 32,
     },
     image:{
-        width:300,
-        height:300,
+        width:100,
+        height:100,
         borderRadius:10,
         borderWidth:2,
         borderColor:'#d35647',
         resizeMode:'contain',
         margin:8
-    }
+    },
 });
 
 interface Props {
@@ -81,6 +86,21 @@ export default function Input(props:Props) {
         price.onChangeText("");
     },[goBack,title,price,url,imageUrl]);
 
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+
+        console.log(result);
+
+        if (!result.cancelled) {
+            imageUrl.onChangeText(result.uri);
+        }
+    };
+
     if(!userState){
         return null;
     }
@@ -96,12 +116,20 @@ export default function Input(props:Props) {
                     <Avatar size={40} source={source}/>
                 </View>
                 <View style={styles.inputContainer}>
-                    <TextField value={title.value} onChangeText={title.onChangeText} secureTextEntry={false} style={styles.text}/>
-                    <TextField value={price.value} onChangeText={price.onChangeText} secureTextEntry={false} keyboardType="numeric" style={styles.text}/>
-                    <TextField value={url.value} onChangeText={onChangeUrl} secureTextEntry={false} style={styles.text}/>
-                    <Image source={{ uri: imageUrl.value }}
-                           style={styles.image}
-                    />
+                    <TextField value={title.value} placeholder="タイトル" onChangeText={title.onChangeText} secureTextEntry={false} style={styles.text}/>
+                    <TextField value={price.value} placeholder="価格" onChangeText={price.onChangeText} secureTextEntry={false} keyboardType="numeric" style={styles.text}/>
+                    <TextField value={url.value} placeholder="販売サイト" onChangeText={onChangeUrl} secureTextEntry={false} style={styles.text}/>
+                    <View style={styles.imageContainer}>
+                        <TouchableOpacity onPress={pickImage}>
+                            <Image source={{ uri: imageUrl.value }} style={styles.image}/>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={pickImage}>
+                            <Image source={{ uri: imageUrl.value }} style={styles.image}/>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={pickImage}>
+                            <Image source={{ uri: imageUrl.value }} style={styles.image}/>
+                        </TouchableOpacity>
+                    </View>
                     <Button label="登録" onPress={onSubmit}/>
                 </View>
             </SafeAreaView>
